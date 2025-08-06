78175a1feec8dc789f72ea87c731c0775853423d
Kotoka Emmanuel <umat@pyclub.org>
2025-08-06 01:32:45 +0000

    All Apps Created

--- Attendance_Project/settings.py
+++ Attendance_Project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'courses.apps.CoursesConfig',
+    'attendance.apps.AttendanceConfig'
 ]
 
 MIDDLEWARE = [
